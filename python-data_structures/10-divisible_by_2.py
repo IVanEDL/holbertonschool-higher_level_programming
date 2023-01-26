56e1f8a3858a354e366b44e9bd84f8faa67398f1
IVanEDL <ivanaq3d@gmail.com>
2023-01-26 08:58:06 -0800

    Task 10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    lista = my_list.copy()
+    for i in range (0, len(my_list)):
+        if my_list[i] % 2 == 0:
+            lista[i] = True
+        else:
+            lista[i] = False
+    return lista
