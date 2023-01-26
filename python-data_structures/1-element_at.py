51f51f8449ee4afbeff07e55cb5d1f8b5c3e37f2
IVanEDL <ivanaq3d@gmail.com>
2023-01-26 05:21:00 -0800

    fixes

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
-    for i in range (0, len(my_list)):
+    for i in range(0, len(my_list)):
         if i == idx:
             return i + 1
