3dafcd71e89f797cd4ea139584cf477307616813
IVanEDL <ivanaq3d@gmail.com>
2023-01-26 08:32:46 -0800

    Task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    a = tuple_a[0] + tuple_b[0]
+    b = tuple_a[1] + tuple_b[1]
+    tup = (a, b)
+    return tup
