f93d2face17e49521fe802617167720310c13c1d
IVanEDL <ivanaq3d@gmail.com>
2023-01-26 08:40:48 -0800

    Task 8 Hotfix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return None
+        tup = (len(sentence), None)
     else:
         tup = (len(sentence), sentence[0])
-        return tup
+    return tup
