bf77d8f0b5c7deae1ee6e1a9a6e91fdf2b710867
IVanEDL <ivanaq3d@gmail.com>
2023-01-26 08:59:35 -0800

    PEP8 Hotfix

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     lista = my_list.copy()
-    for i in range (0, len(my_list)):
+    for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
             lista[i] = True
         else:
