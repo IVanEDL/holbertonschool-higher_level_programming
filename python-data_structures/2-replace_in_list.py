bb847a67356aa5028fac267912bd3d8aa336b60f
IVanEDL <ivanaq3d@gmail.com>
2023-01-26 05:34:43 -0800

    Task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,6 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return my_list
-    for i in range(0, len(my_list)):
-        if i == idx:
-            my_list[i] = element
-            return my_list
+    else:
+        my_list[idx] = element
+        return my_list
