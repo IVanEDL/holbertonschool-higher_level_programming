d437e6b7344e287dff7f1c4e4299fd922bcf3ae6
IVanEDL <ivanaq3d@gmail.com>
2023-01-29 14:59:09 -0800

    Task 12 Test 3

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    roman_dicc = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dicc_sub = {'IV', 'IX', 'XL', 'XC', 'CD', 'CM'}
+    result = 0
+    prev = ""
+    for numeral in roman_string:
+        if prev + numeral in roman_dicc_sub:
+            result = result - roman_dicc.get(prev) * 2
+        result = result + roman_dicc.get(numeral)
+        prev = numeral
+    return result
