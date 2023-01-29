f57cf8ef083ef9feed76e4f8957a18f5ac18be5f
IVanEDL <ivanaq3d@gmail.com>
2023-01-29 15:00:51 -0800

    Hotfix: Betty 2

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
-    roman_dicc = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dicc = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+            'D': 500, 'M': 1000}
     roman_dicc_sub = {'IV', 'IX', 'XL', 'XC', 'CD', 'CM'}
     result = 0
     prev = ""
