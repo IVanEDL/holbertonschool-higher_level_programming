734f4d953afc04b93ef492a4be2e58775e453715
IVanEDL <ivanaq3d@gmail.com>
2023-01-31 09:09:24 -0800

    Task 6 Test 3

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,9 +13,9 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if isinstance(position, tuple) is False or len(position) is not 2 or\
-        not all(isinstance(az, int) and az >= 0 for az in position):
-                   raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(position, tuple) is False or len(position) is not 2 or \
+                not all(isinstance(az, int) and az >= 0 for az in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
         """returns area of the square"""
@@ -44,16 +44,18 @@ class Square:
     def position(self, position):
         """position setter"""
         self.__position = position
-        if isinstance(position, tuple) is False or len(position) is not 2 or\
-        not all(isinstance(az, int) and az >= 0 for az in position):
-                   raise TypeError("position must be a tuple of 2 positive integers")
+        if isinstance(position, tuple) is False or len(position) is not 2 or \
+                not all(isinstance(az, int) and az >= 0 for az in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
         """prints square"""
         if self.__size == 0:
             print("")
+        for erin in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
-            for p in range(self.__position[1]):
+            for p in range(self.__position[0]):
                 print(" ", end="")
             for a in range(self.__size):
                 print("#", end="")
