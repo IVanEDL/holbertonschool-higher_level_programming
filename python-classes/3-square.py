e8e81c7771f6272db599e9c9f0f97328f26e7ce7
IVanEDL <ivanaq3d@gmail.com>
2023-01-31 05:47:05 -0800

    Task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """Square: Class corresponding to a square"""
+    def __init__(self, size=0):
+        """initializes method
+        """
+        self.__size = size
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """returns area of the square"""
+        return Square.__size ** 2
