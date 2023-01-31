0efed3387caf46dc93dc278cec601a699994f51b
IVanEDL <ivanaq3d@gmail.com>
2023-01-31 05:44:02 -0800

    Task 2 Test 6

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """Square: Class corresponding to a square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """initializes method
         """
         self.__size = size
-        if isinstance(size, int) == False:
+        if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
