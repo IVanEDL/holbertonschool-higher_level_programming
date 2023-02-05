d0306345824c50aca77153f081e2ff7f64a5d5e1
IVanEDL <ivanaq3d@gmail.com>
2023-02-05 13:55:57 -0800

    Task 2, Test 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""prints the name given"""
-
+"""
+prints the name given
+"""
 
 def say_my_name(first_name, last_name=""):
-    """prints a string conformed of the given parts, first checking for
-    strings"""
+    """
+    prints a string conformed of the given parts, first checking for
+    strings
+    """
     if not first_name or isinstance(first_name, str) is False:
         raise TypeError("first_name must be a string")
     elif isinstance(last_name, str) is False:
         raise TypeError("last_name must be a string")
     else:
-        print(f"My name is {first name} {last name}")
+        print(f"My name is {first_name} {last_name}")
