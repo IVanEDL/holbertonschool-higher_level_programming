1d5ee689614d48229f744d2eb6ac8d7afa513679
IVanEDL <ivanaq3d@gmail.com>
2023-02-12 15:12:11 -0800

    T9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Task 8"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """RECTANGOL class 2k23"""
+    def __init__(self, width, height):
+        """initialices class"""
+        BaseGeometry.integer_validator(self, "width", width)
+        self.__width = width
+        BaseGeometry.integer_validator(self, "height", height)
+        self.__height = height
+
+    def __str__(self):
+        """Rectangle representation"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__height * self.__width
