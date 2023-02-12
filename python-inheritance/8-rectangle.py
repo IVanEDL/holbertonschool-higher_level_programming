1d1f4b1f4ec5f9f9f964d81511067da464df646b
IVanEDL <113697698+IVanEDL@users.noreply.github.com>
2023-02-12 20:04:25 -0300

    aaaa

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Task 8"""
 
-
-BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
