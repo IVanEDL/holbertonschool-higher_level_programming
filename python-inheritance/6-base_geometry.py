fcf63fe8a2fb79ccee1ad13099e74de08d1b52ea
IVanEDL <ivanaq3d@gmail.com>
2023-02-12 14:34:57 -0800

    Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Base Geometry"""
+
+
+class BaseGeometry:
+    """Class of geometyr"""
+    def area(self):
+        """Does the area"""
+        raise Exception("area() is not implemented")
