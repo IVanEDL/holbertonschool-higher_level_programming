2fc0818afee8ffd18984aae689b474fd07401df4
IVanEDL <ivanaq3d@gmail.com>
2023-02-12 15:59:00 -0800

    Task 2 T2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Task 2"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """Writes a file."""
     with open(filename, 'a', encoding='utf-8') as i:
         return i.write(text)
