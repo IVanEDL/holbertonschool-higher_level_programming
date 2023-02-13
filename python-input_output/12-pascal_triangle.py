8f685fa0ee71e0dce2fd007a61afbd430aa9a03b
IVanEDL <ivanaq3d@gmail.com>
2023-02-12 17:14:06 -0800

    Task 12 T3

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
         return []
     else:
         triangle = [[1]]
-    for i in range(1, n - 1):
+    for i in range(0, n - 1):
         aux = [1]
         for p in range(1, i + 1):
             aux.append(triangle[i][p] + triangle[i][p - 1])
