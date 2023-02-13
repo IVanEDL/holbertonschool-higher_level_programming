7c38b9cb196c09a46c6165d641e47f009d16c2b0
IVanEDL <ivanaq3d@gmail.com>
2023-02-12 16:35:24 -0800

    Task 7 Fix pep8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,16 +2,16 @@
 """TASK 7"""
 
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 from os import path
 from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if path.exists('add_item.json'):
     my_list = load_from_json_file('add_item.json')
 else:
     my_list = []
-for i in range (1, len(argv)):
+for i in range(1, len(argv)):
     my_list.append(argv[i])
 save_to_json_file(my_list, 'add_item.json')
