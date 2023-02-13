76ea422826f390f15c1077cb658595c1415623fd
IVanEDL <ivanaq3d@gmail.com>
2023-02-12 17:11:54 -0800

    Task 12 T2

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Task 12. Oh man."""
 
+
 def pascal_triangle(n):
     """does the pascal triangly thing"""
     if n <= 0:
         return []
     else:
         triangle = [[1]]
-        for i in range(1, n - 1):
-            aux = [1]
-            for p in range(1, i + 1):
-                aux.append(triangle[i][p] + triangle[i][p - 1])
-            aux.append(1)
-            triangle.append(aux)
-        return triangle
+    for i in range(1, n - 1):
+        aux = [1]
+        for p in range(1, i + 1):
+            aux.append(triangle[i][p] + triangle[i][p - 1])
+        aux.append(1)
+        triangle.append(aux)
+    return triangle
