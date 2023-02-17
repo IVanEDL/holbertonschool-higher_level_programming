426a26e95689af5350ee5180d8be0879835f6126
IVanEDL <ivanaq3d@gmail.com>
2023-02-17 13:32:42 -0800

    Square fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,8 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        self.__size = size
+        super().__init__(self.__size, self.__size, x, y, id)
 
     def __str__(self):
         """str return method"""
