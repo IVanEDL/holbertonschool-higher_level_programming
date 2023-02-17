5d918f987e06fed4222748de91156b6bd02c2872
IVanEDL <ivanaq3d@gmail.com>
2023-02-17 13:37:06 -0800

    A ver ahora

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(self, size, size, x, y, id)
+        super().__init__(size, size.copy(), x, y, id)
 
     def __str__(self):
         """str return method"""
