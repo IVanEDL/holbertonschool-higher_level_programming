60c78bfad73c63f1db7a6201357dd3cea59d7127
IVanEDL <ivanaq3d@gmail.com>
2023-02-17 09:47:10 -0800

    Task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base class ig"""
+
+
+class Base:
+    """This is a base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=none):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
