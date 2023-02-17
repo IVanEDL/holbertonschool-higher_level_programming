5c50230a75d747e1302f92cc574521c2b1856c6a
IVanEDL <113697698+IVanEDL@users.noreply.github.com>
2023-02-17 15:43:52 -0300

    Now yes. Task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -36,7 +36,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """width setter"""
-        self.__width = width
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
 
     @property
     def height(self):
@@ -46,7 +51,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """height setter"""
-        self.__height = height
+        if type(width) is not int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
 
     @property
     def x(self):
@@ -56,7 +66,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """x setter"""
-        self.__x = x
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
 
     @property
     def y(self):
@@ -66,4 +81,9 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """y setter"""
-        self.__y = y
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y
