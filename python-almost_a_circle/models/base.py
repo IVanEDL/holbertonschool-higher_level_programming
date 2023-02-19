3cbdd28db375c081dd91e4c441c23e4e60b56d60
IVanEDL <ivanaq3d@gmail.com>
2023-02-19 15:36:20 -0800

    Fix 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -37,4 +37,4 @@ class Base:
         """returns the list of the JSON string representation json_string"""
         if json_string is None or len(json_string is 0:
             return []
-        return json_load(json_string)
+        return json_loads(json_string)
