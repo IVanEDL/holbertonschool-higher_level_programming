774c9db7009e62f39f276f620433824fd5f5df79
IVanEDL <ivanaq3d@gmail.com>
2023-02-19 15:25:30 -0800

    Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Base class ig"""
+import json
+from os import path
 
 
 class Base:
@@ -12,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns JSON list representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) is 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
