0b020e8efcc88f88679d5b4dc32a4c731ca35ec2
root <IVanEDL>
2023-03-16 09:47:11 -0700

    Fixes task 1-2

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE hbtn_0c_0 IF NOT EXISTS;
--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- deletes the database hbtn_0c_0 in your MySQL server.
-DROP DATABASE hbtn_0c_0;
+DROP DATABASE hbtn_0c_0 IF EXISTS;
