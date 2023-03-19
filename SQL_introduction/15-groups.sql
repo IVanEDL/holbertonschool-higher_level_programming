f48e294fa63ac413d15d3c796ddcd010c8aedf3c
IVanEDL <ivanaq3d@gmail.com>
2023-03-19 14:57:31 -0300

    Task 15 fix: Table confusion

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT score, COUNT(score) AS number
-FROM first_table
+FROM second_table
 GROUP BY score
 ORDER BY score DESC;
