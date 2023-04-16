e3f22cb11c73a91c42debd6c703a09472d2bfec2
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 15:05:46 -0700

    Fixes part 2: semistandard

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const tsil = [];
-    let i = 0;
-    for (let p = list.length - 1; p <= 0; p--) {
-        tsil[i] = list[p];
-        i++;
-    }
-    return tsil;
+  const tsil = [];
+  let i = 0;
+  for (let p = list.length - 1; p <= 0; p--) {
+    tsil[i] = list[p];
+    i++;
+  }
+  return tsil;
 };
