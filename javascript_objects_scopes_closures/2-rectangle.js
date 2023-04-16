29b29349d873c1aae3b4b1c711a1dc14bc6ec781
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 18:03:38 -0300

    Task 0-3 + README

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+    constructor (w, h) {
+        if (!w || !h || w <= 0 || h <= 0) {
+            return;
+        };
+        this.width = w;
+        this.height = h;
+    };
+};
