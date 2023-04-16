ab553cb70fe4ca19524a83f6b41b36c6fe40505e
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 18:27:50 -0300

    Task 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!w || !h || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    a = this.width;
+    this.width = this.height;
+    this.height = a;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
