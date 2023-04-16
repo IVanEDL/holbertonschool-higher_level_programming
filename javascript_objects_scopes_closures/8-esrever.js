8b4d62d620e3535d7b1257a72d9c93dced346eee
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 19:08:36 -0300

    Task 8 last fixes

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const tsil = [];
-  let i = 0;
-  for (let p = list.length - 1; p <= 0; p--) {
+  for (let p = list.length - 1, i = 0; p >= 0; p--) {
     tsil[i] = list[p];
     i++;
   }
