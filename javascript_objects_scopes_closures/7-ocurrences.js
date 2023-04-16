787f384e708be271e7a46693d7e578f1dd163472
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 14:54:50 -0700

    Standardfixes 7

--- javascript_objects_scopes_closures/7-ocurrences.js
+++ javascript_objects_scopes_closures/7-ocurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let i = 0;
-    for (const p in list) {
-        if (list[p] === searchElement) {
-            i += 1;
-        }
+  let i = 0;
+  for (const p in list) {
+    if (list[p] === searchElement) {
+      i += 1;
     }
-    return i;
+  }
+  return i;
 };
