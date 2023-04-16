dc68f80a97517667e9c1b79267e08cd7238121bc
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 14:42:19 -0700

    Task 6 semistandard

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
-    constructor (size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint (c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.size; i++) {
-            console.log(c.repeat(this.size));
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.size; i++) {
+      console.log(c.repeat(this.size));
+    }
+  }
 };
