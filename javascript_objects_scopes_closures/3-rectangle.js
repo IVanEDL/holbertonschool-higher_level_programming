b0697c7f7bf49b04ba80d2a79ae21ebc940046c4
IVanEDL <ivanaq3d@gmail.com>
2023-04-16 14:06:20 -0700

    Semistandard fixes

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-        if (!w || !h || w <= 0 || h <= 0) {
-            return;
-        };
-        this.width = w;
-        this.height = h;
-    };
+  constructor (w, h) {
+    if (!w || !h || w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
 
-    print () {
-        for (let i = 0; i < this.height; i++) {
-            for (let p = 0; i < this.width; p++) {
-                process.stdout.write("X");
-            };
-            process.stdout.write("\n");
-        };
-    };
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let p = 0; i < this.width; p++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
 };
