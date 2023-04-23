b553b02e0f457443ae762a452b21418e2bc20249
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 20:01:23 -0300

    Task 4 remake + fix, check for sstandard and test

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,14 @@
 const args = process.argv.slice(2);
 const request = require('request');
 let thatGuy = args[0];
-if (args[0] === 'https://swapi-api.hbtn.io/api/films/') {
-  thatGuy = 'https://swapi-api.alx-tools.com/api/people/18/';
+if (args[0] = 'https://swapi-api.hbtn.io/api/films/') {
+    thatGuy = 'https://swapi-api.alx-tools.com/api/people/18/';
 }
-request.get(thatGuy, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    const data = JSON.parse(body).films.length;
-    console.log(data);
-  } else {
-    console.log(error);
-  }
+request(thatGuy, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        console.log(JSON.parse(body).films.length);
+    }
+    else {
+        console.log(error);
+    }
 });
