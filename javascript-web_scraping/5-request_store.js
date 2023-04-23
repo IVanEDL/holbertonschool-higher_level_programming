c6b8e7a81c8041b1baf46c79144780cb0a9e8e97
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 15:46:39 -0700

    Task added lmao

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,10 +2,10 @@
 const args = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs');
-request.get(args[0], function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        fs.writeFile(args[1], body);
-    } else {
-        console.log(error);
-    }
+request.get(args[0], function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(args[1], body);
+  } else {
+    console.log(error);
+  }
 });
