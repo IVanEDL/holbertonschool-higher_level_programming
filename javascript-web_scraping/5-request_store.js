22142101204ba24b5b1836192dfacc8b21ad8bbc
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 15:55:07 -0700

    Task 5 semistandard

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 request.get(args[0], function (error, response, body) {
   if (!error && response.statusCode === 200) {
     fs.writeFile(args[1], body, 'utf-8', (err) => {
-        if (err) throw err;
-      });
-    }
-  else console.log(error);
+      if (err) throw err;
+    });
+  } else console.log(error);
 });
