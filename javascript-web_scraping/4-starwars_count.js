a866b871b2867f8aa5e345cb752635749d93521f
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 16:05:00 -0700

    Task 4 fix semistandard and some minor mistakes

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,13 @@
 const args = process.argv.slice(2);
 const request = require('request');
 let thatGuy = args[0];
-if (args[0] = 'https://swapi-api.hbtn.io/api/films/') {
-    thatGuy = 'https://swapi-api.alx-tools.com/api/people/18/';
+if (args[0] === 'https://swapi-api.hbtn.io/api/films/') {
+  thatGuy = 'https://swapi-api.alx-tools.com/api/people/18/';
 }
 request(thatGuy, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        console.log(JSON.parse(body).films.length);
-    }
-    else {
-        console.log(error);
-    }
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(body).films.length);
+  } else {
+    console.log(error);
+  }
 });
