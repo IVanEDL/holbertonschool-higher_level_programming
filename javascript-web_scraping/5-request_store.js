0bbe96e029994b626b546b63d2b5df091db5d134
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 19:52:45 -0300

    Task 5 fix

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,8 +4,9 @@ const request = require('request');
 const fs = require('fs');
 request.get(args[0], function (error, response, body) {
   if (!error && response.statusCode === 200) {
-    fs.writeFile(args[1], body);
-  } else {
-    console.log(error);
-  }
+    fs.writeFile(args[1], body, 'utf-8', (err) => {
+        if (err) throw err;
+      });
+    }
+  else console.log(error);
 });
