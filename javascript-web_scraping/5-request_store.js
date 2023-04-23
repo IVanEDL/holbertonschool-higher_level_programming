46002248033c733c95452bddbfad4fab985e8bb8
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 19:44:31 -0300

    Great so task 5 didnt save

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+request.get(args[0], function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        fs.writeFile(args[1], body);
+    } else {
+        console.log(error);
+    }
+});
