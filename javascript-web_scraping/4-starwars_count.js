32957038af8838c29eb13ae605558c66cc430efc
IVanEDL <ivanaq3d@gmail.com>
2023-04-23 20:08:32 -0300

    Wtf is happening to the checker

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 const request = require('request');
 let thatGuy = args[0];
 if (args[0] === 'https://swapi-api.hbtn.io/api/films/') {
-  thatGuy = 'https://swapi-api.alx-tools.com/api/people/18/';
+  thatGuy = 'https://swapi-api.hbtn.io/api/people/18/';
 }
 request(thatGuy, (error, response, body) => {
   if (!error && response.statusCode === 200) {
